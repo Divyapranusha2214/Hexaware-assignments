b74a7101c0366ea0e24693090cb0aa54820bdb0f
Divyapranusha2214 <106370731+Divyapranusha2214@users.noreply.github.com>
2023-02-14 19:27:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Sclmg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NonTeachingstaff nonteachingstaff = new NonTeachingstaff(111, "di", "palasa,srikakulam(dist)", 934587902, "Btech", 80000, "CSE", 45);
+            Console.WriteLine(nonteachingstaff.Getdetails());
+
+
+            Teachingstaff teachingstaff = new Teachingstaff(112, "divya", "tekkali,srkakulam(dist)", 2774785567, "M.Tech", 90000, "B.Tech", "EEE");
+
+            Console.WriteLine(teachingstaff.Getdetails());
+
+            Student student = new Student(12, "LILLY", "CHINNABAdam,srikakulam(dist)", 93986495, 4, 55.99f, "Z", 10000.00f);
+            Console.WriteLine(student.Getdetails());
+        }
+    }
+}
